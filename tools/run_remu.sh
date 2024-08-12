68cfc78357e01478fdceb081a902749206089484
castermode <castermode@gmail.com>
2024-08-12 14:26:18 +0800

    add code

---
+++ tools/run_remu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for file in `ls remu_*_test`; do
+    echo "Running $file..."
+
+    ./"$file"
+    ret=$?
+    if [ ${ret} -ne 0 ]; then
+        echo "exit, return ${ret}"
+        exit 1
+    fi
+
+    ./"$file --enable-pre-vote"
+    ret=$?
+    if [ ${ret} -ne 0 ]; then
+        echo "exit, return ${ret}"
+        exit 1
+    fi
+done
