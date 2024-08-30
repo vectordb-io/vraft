5c334d62121b13825ef9e8a42bfac337f9eafefe
castermode <castermode@gmail.com>
2024-08-30 12:45:22 +0800

    add code

--- output/test/t.sh
+++ output/test/t.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sh run_all_remu.sh --params=./remu_test2.param --save=yes > runlog
-dirpath=`cat runlog | tail -n1`
+sh run_all_remu.sh --params=./remu_test2.param --save=yes > execute_log
+dirpath=`cat execute_log | tail -n1`
 
-sh /tmp/vraft_tools/get_result.sh ./runlog > ./result
-sh /tmp/vraft_tools/generate_text_html.sh ./runlog > ${dirpath}/runlog.html
-sh /tmp/vraft_tools/generate_text_html.sh ./result > ${dirpath}/result.html
-mv ./runlog ${dirpath}
-mv ./result ${dirpath}
+sh /tmp/vraft_tools/get_result.sh ./execute_log > ./execute_result
+sh /tmp/vraft_tools/generate_text_html.sh ./execute_log > ${dirpath}/execute_log.html
+sh /tmp/vraft_tools/generate_text_html.sh ./execute_result > ${dirpath}/execute_result.html
+mv ./execute_log ${dirpath}
+mv ./execute_result ${dirpath}
 
 cd /tmp/vraft_tools/
 sh generate_casedir.sh ${dirpath}
